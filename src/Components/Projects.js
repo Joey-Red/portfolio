174d883c4bb02991809d7a79f9dd18c6c066516c
Joey <xjoey96@gmail.com>
2022-10-07 19:28:08 -0500

    ui bug

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -9,89 +9,89 @@ import { motion } from "framer-motion";
 import NavIntro from "./NavIntro";
 
 function Projects(props) {
-  let {
-    setShowProjects,
-    setShowSkills,
-    setShowContact,
-    setShowNavIntro,
-    setIsOpen,
-    ranNav,
-    setRanNav,
-  } = props;
-  let [project, setProject] = useState(0);
-  let handleChange = (e, p) => {
-    setProject(p);
-  };
-  let projectSwitch = () => {
-    if (project === 0) {
-      return <Project1 />;
-    }
-    if (project === 1) {
-      return <Project2 />;
-    }
-    if (project === 2) {
-      return <Project3 />;
-    }
-  };
-  function a11yProps(project) {
-    return {
-      id: `simple-tab-${project}`,
-      "aria-controls": `simple-tabpanel-${project}`,
+    let {
+        setShowProjects,
+        setShowSkills,
+        setShowContact,
+        setShowNavIntro,
+        setIsOpen,
+        ranNav,
+        setRanNav,
+    } = props;
+    let [project, setProject] = useState(0);
+    let handleChange = (e, p) => {
+        setProject(p);
     };
-  }
-  return (
-    <>
-      <NavIntro
-        setShowProjects={setShowProjects}
-        setShowSkills={setShowSkills}
-        setShowContact={setShowContact}
-        setShowNavIntro={setShowNavIntro}
-        setIsOpen={setIsOpen}
-        ranNav={ranNav}
-        setRanNav={setRanNav}
-      />
-      <motion.div
-        className="projectOuter"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-      >
-        <Container
-          maxWidth="sm"
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-          id="projects"
-        >
-          <Stack
-            spacing={2}
-            style={{
-              background: "white",
-              padding: "10px 30px",
-              marginBottom: "-4px",
-              maxWidth: "100%",
-              minWidth: "100%",
-              alignItems: "center",
-            }}
-            className="pagStack"
-          >
-            <Tabs
-              value={project}
-              onChange={handleChange}
-              aria-label="Projects 1-3"
+    let projectSwitch = () => {
+        if (project === 0) {
+            return <Project1 />;
+        }
+        if (project === 1) {
+            return <Project2 />;
+        }
+        if (project === 2) {
+            return <Project3 />;
+        }
+    };
+    function a11yProps(project) {
+        return {
+            id: `simple-tab-${project}`,
+            "aria-controls": `simple-tabpanel-${project}`,
+        };
+    }
+    return (
+        <>
+            <NavIntro
+                setShowProjects={setShowProjects}
+                setShowSkills={setShowSkills}
+                setShowContact={setShowContact}
+                setShowNavIntro={setShowNavIntro}
+                setIsOpen={setIsOpen}
+                ranNav={ranNav}
+                setRanNav={setRanNav}
+            />
+            <motion.div
+                className="projectOuter"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
             >
-              <Tab label="Event Planner" {...a11yProps(0)} />
-              <Tab label="BlogApi" {...a11yProps(1)} />
-              <Tab label="Reddit" {...a11yProps(2)} />
-            </Tabs>
-          </Stack>
-          <Card>{projectSwitch(project)}</Card>
-        </Container>
-      </motion.div>
-    </>
-  );
+                <Container
+                    maxWidth="sm"
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                    }}
+                    id="projects"
+                >
+                    <Stack
+                        spacing={2}
+                        style={{
+                            background: "white",
+                            padding: "10px 30px",
+                            marginBottom: "-4px",
+                            maxWidth: "100%",
+                            minWidth: "100%",
+                            alignItems: "center",
+                        }}
+                        className="pagStack"
+                    >
+                        <Tabs
+                            value={project}
+                            onChange={handleChange}
+                            aria-label="Projects 1-3"
+                        >
+                            <Tab label="Event Planner" {...a11yProps(0)} />
+                            <Tab label="BlogApi" {...a11yProps(1)} />
+                            <Tab label="Reddit" {...a11yProps(2)} />
+                        </Tabs>
+                    </Stack>
+                    <Card>{projectSwitch(project)}</Card>
+                </Container>
+            </motion.div>
+        </>
+    );
 }
 
 export default Projects;
